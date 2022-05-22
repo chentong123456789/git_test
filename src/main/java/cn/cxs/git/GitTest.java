b139ca3200f7e4252c0eeee2afb81f3adb50f0b1
atongmu123456 <18341358747@163.com>
2022-05-22 16:09:46 +0800

    hot-fix commit-1

--- src/main/java/cn/cxs/git/GitTest.java
+++ src/main/java/cn/cxs/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("hello cxs");
         System.out.println("hello cxs2");
         System.out.println("hello cxs3");
+        System.out.println("hello cxs4");
 
     }
 }
